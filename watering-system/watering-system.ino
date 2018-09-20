be0930e561d45ab5bb3f1fa27bb7b44874131da0
Roberto Hojman <rhojman@gmail.com>
2018-09-20 01:04:20 -0300

    Múltiples plantas
    Múltiples plantas

---
+++ watering-system/watering-system.ino
@@ -0,0 +1,74 @@
+#include <Servo.h>
+//Define el pin de control de la bomba de agua
+const int pumpAnodePin =  6;      //pin 6 conectado al ánodo de la bomba de agua
+const int pumpCathodePin =  7;   //pin 7 conectado al cátodo de la bomba de agua
+
+Servo servoH;  // crea un objeto servo Horizontal
+Servo servoV;  // crea un objeto servo Vertical
+int pinHumed1 = 0;  // pin análogo para sensor de humedad 1
+int pinHumed2 = 1;  // pin análogo para sensor de humedad 2
+int pinHumed3 = 2;  // pin análogo para sensor de humedad 3
+int humed1;    // variable para leer el valor del sensor 1
+int humed2;    // variable para leer el valor del sensor 2
+int humed3;    // variable para leer el valor del sensor 3
+int servo_humed1;
+int sensor_list[] = {pinHumed1, pinHumed2, pinHumed3};
+
+int umbralInf_1 = 50; // cota inferior de humedad en porcentaje sensor 1
+int umbralInf_2 = 50; // cota inferior de humedad en porcentaje sensor 2
+int umbralInf_3 = 50; // cota inferior de humedad en porcentaje sensor 3
+
+int umbralSup_1 = 70; // cota superior de humedad en porcentaje sensor 1
+int umbralSup_2 = 80; // cota superior de humedad en porcentaje sensor 2
+int umbralSup_3 = 80; // cota superior de humedad en porcentaje sensor 3
+int umbral_humed1;
+
+int angulo_inicial = 0;
+int angulo_1 = 30;
+int angulo_2 = 90;
+int angulo_3 = 150;
+int angulo_list[] = {angulo_1, angulo_2, angulo_3};
+
+void setup() 
+{  
+Serial.begin(9600);
+servoH.attach(9);  // relaciona el pin 9 al objeto servo horizontal
+servoV.attach(10);  // relaciona el pin 10 al objeto servo vertical 
+pinMode(pumpAnodePin, OUTPUT);
+pinMode(pumpCathodePin, OUTPUT);
+servoH.write(angulo_inicial);
+}
+
+void loop() 
+{  
+regar(0);
+}
+
+
+void regar(int j){
+  while(j < (sizeof(sensor_list)/sizeof(int))){   
+    int i=j;
+    humed1 = analogRead(sensor_list[i]);            // lee el valor del sensor de humedad 
+    umbral_humed1 = map(humed1, 0, 1023, 100, 0);
+    Serial.print("Humedad: ");
+    Serial.print(umbral_humed1);
+    Serial.print("%");
+    delay(1000);
+  
+    if((umbral_humed1 >= umbralInf_1 && umbral_humed1 <= umbralSup_1) || (umbral_humed1 > umbralSup_1)){
+      Serial.print("  (bomba off)");
+      i++;
+      regar(i);  
+      }  
+    if(umbral_humed1 < umbralInf_1){
+      servoH.write(angulo_list[i]); 
+      delay(1000);
+      digitalWrite(pumpAnodePin, HIGH);    
+      delay(5000); 
+      digitalWrite(pumpAnodePin, LOW);
+      delay(1000);   
+      regar(i);  
+    }
+    servoH.write(angulo_inicial); 
+  }
+}
